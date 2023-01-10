165a6d324ef62cd4121e73bd5a18e5ca4fb2f19e
Hassan <hasanahmad9812021@gmail.com>
2023-01-10 14:44:02 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(const MyApp());
 }
 
@@ -19,18 +19,20 @@ class MyApp extends StatelessWidget {
 class AppsHome extends StatelessWidget {
   const AppsHome({Key? key}) : super(key: key);
 
-  // SnackBar message code
-  SnackBarMessage(message,context){
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message))
-    );
+  /// SnackBar message code
+  SnackBarMessage(message, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      /// AppBar
       appBar: AppBar(
-        title: Text("Flutter with Hasan"),
+        /// appBar
+        title: Text("Mess App"),
         //titleSpacing: 15,
         centerTitle: true,
         toolbarOpacity: 1,
@@ -38,21 +40,54 @@ class AppsHome extends StatelessWidget {
         elevation: 25,
         backgroundColor: Colors.blueAccent,
 
-        // set action icon in appbar
+        /// set action icon in appbar
         actions: [
-          IconButton(onPressed: (){
-            SnackBarMessage('Search',context);},
-              icon: Icon(Icons.search)),
-          IconButton(onPressed: (){SnackBarMessage('Call', context);},
+          IconButton(
+            onPressed: () {
+              SnackBarMessage('Search', context);
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+              onPressed: () {
+                SnackBarMessage('Call', context);
+              },
               icon: Icon(Icons.add_call)),
-          IconButton(onPressed: (){SnackBarMessage('Settings', context);},
+          IconButton(
+              onPressed: () {
+                SnackBarMessage('Settings', context);
+              },
               icon: Icon(Icons.settings_applications))
         ],
       ),
 
-      body: Center(
-        child: Text('Welcome to learning flutter with me'),
+      /// bottom navigation bar / tab
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        selectedFontSize: 18,
+        unselectedFontSize: 8,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notification_add_sharp), label: 'Notification'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.read_more), label: 'Read more')
+        ],
+        onTap: (int index) {
+          if (index == 0) {
+            SnackBarMessage('See Notifications', context);
+          }
+          if (index == 1) {
+            SnackBarMessage('Goto Home', context);
+          }
+          if (index == 2) {
+            SnackBarMessage('Read  more', context);
+          }
+        },
       ),
+
+
     );
   }
 }
