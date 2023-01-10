f3a61abe207d8d94efbded33dca29bf8e12912d4
Hassan <hasanahmad9812021@gmail.com>
2023-01-10 14:44:02 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(const MyApp());
 }
 
@@ -19,18 +19,20 @@ class MyApp extends StatelessWidget {
 class AppsHome extends StatelessWidget {
   const AppsHome({Key? key}) : super(key: key);
 
-  // SnackBar message code
-  SnackBarMessage(message,context){
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message))
-    );
+  /// SnackBar message code
+  SnackBarMessage(message, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      /// AppBar
       appBar: AppBar(
-        title: Text("Flutter with Hasan"),
+        /// appBar
+        title: Text("Mess App"),
         //titleSpacing: 15,
         centerTitle: true,
         toolbarOpacity: 1,
@@ -38,21 +40,85 @@ class AppsHome extends StatelessWidget {
         elevation: 25,
         backgroundColor: Colors.blueAccent,
 
-        // set action icon in appbar
+        /// set action icon in appbar
         actions: [
-          IconButton(onPressed: (){
-            SnackBarMessage('Search',context);},
-              icon: Icon(Icons.search)),
-          IconButton(onPressed: (){SnackBarMessage('Call', context);},
+          IconButton(
+            onPressed: () {
+              SnackBarMessage('Search', context);
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+              onPressed: () {
+                SnackBarMessage('Call', context);
+              },
               icon: Icon(Icons.add_call)),
-          IconButton(onPressed: (){SnackBarMessage('Settings', context);},
+          IconButton(
+              onPressed: () {
+                SnackBarMessage('Settings', context);
+              },
               icon: Icon(Icons.settings_applications))
         ],
       ),
 
-      body: Center(
-        child: Text('Welcome to learning flutter with me'),
+      /// Navigation Drawer
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.all(0),
+                child: UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(color: Colors.blueGrey),
+                  accountName: Text('Mess Name', style: TextStyle(color: Colors.white),),
+                  accountEmail: Text('messSylhet2023@gmail.com', style: TextStyle(color: Colors.white)),
+                  currentAccountPicture: Image.network('https://cdn0.iconfinder.com/data/icons/education-school-science/100/29-512.png'),
+                  //currentAccountPictureSize: Size.square(150),
+                )
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('My Profile'),
+              hoverColor: Colors.grey,
+              onTap: () {
+                SnackBarMessage('My Profile', context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              hoverColor: Colors.grey,
+              onTap: () {
+                SnackBarMessage('Goto Home', context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.call),
+              title: Text('Contact with Manager'),
+              hoverColor: Colors.grey,
+              onTap: () {
+                SnackBarMessage('Call to Manager', context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.report_gmailerrorred_outlined),
+              title: Text('Report to Manager'),
+              hoverColor: Colors.grey,
+              onTap: () {
+                SnackBarMessage('Report to Manager', context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.question_mark),
+              title: Text('About'),
+              hoverColor: Colors.grey,
+              onTap: () {
+                SnackBarMessage('About Us', context);
+              },
+            ),
+          ],
+        ),
       ),
+
     );
   }
 }
