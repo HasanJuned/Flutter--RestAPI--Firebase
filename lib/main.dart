8da875d6a53c7104a7a7e00b921a19dd19588597
Hassan <hasanahmad9812021@gmail.com>
2023-01-10 14:44:02 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(const MyApp());
 }
 
@@ -19,18 +19,20 @@ class MyApp extends StatelessWidget {
 class AppsHome extends StatelessWidget {
   const AppsHome({Key? key}) : super(key: key);
 
-  // SnackBar message code
-  SnackBarMessage(message,context){
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message))
-    );
+  /// SnackBar message code
+  SnackBarMessage(message, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      /// AppBar
       appBar: AppBar(
-        title: Text("Flutter with Hasan"),
+        /// appBar
+        title: Text("Mess App"),
         //titleSpacing: 15,
         centerTitle: true,
         toolbarOpacity: 1,
@@ -38,21 +40,68 @@ class AppsHome extends StatelessWidget {
         elevation: 25,
         backgroundColor: Colors.blueAccent,
 
-        // set action icon in appbar
+        /// set action icon in appbar
         actions: [
-          IconButton(onPressed: (){
-            SnackBarMessage('Search',context);},
-              icon: Icon(Icons.search)),
-          IconButton(onPressed: (){SnackBarMessage('Call', context);},
+          IconButton(
+            onPressed: () {
+              SnackBarMessage('Search', context);
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+              onPressed: () {
+                SnackBarMessage('Call', context);
+              },
               icon: Icon(Icons.add_call)),
-          IconButton(onPressed: (){SnackBarMessage('Settings', context);},
+          IconButton(
+              onPressed: () {
+                SnackBarMessage('Settings', context);
+              },
               icon: Icon(Icons.settings_applications))
         ],
       ),
 
-      body: Center(
-        child: Text('Welcome to learning flutter with me'),
+      /// Body
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        //crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+
+          /// Container
+          Container(
+            height: 200,
+            width: 200,
+            padding: EdgeInsets.all(5),
+            margin: EdgeInsets.all(20),
+            //color: Colors.redAccent,
+            child: Text('This is Container', style: TextStyle(fontSize: 24)),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.greenAccent, width: 5),
+              color: Colors.redAccent,
+            ),
+          ),
+          Container( /// adding image in this container
+            height: 200,
+            width: 200,
+            padding: EdgeInsets.all(5),
+            margin: EdgeInsets.all(20),
+            //color: Colors.redAccent,
+            child: Image.network("https://cdn0.iconfinder.com/data/icons/education-school-science/100/29-512.png"),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.greenAccent, width: 5),
+              color: Colors.redAccent,
+            ),
+          ),
+
+        ],
       ),
+
+
+
     );
   }
 }
