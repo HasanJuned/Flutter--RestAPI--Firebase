3d21c468c9810bfb279205f8cdd4c095152391ee
Hassan <hasanahmad9812021@gmail.com>
2023-01-10 14:44:02 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,77 @@
 import 'package:flutter/material.dart';
 
-
-/// This is practise file not mandatory
 main() {
-  runApp(HelloWorldApp());
+  runApp(const MyApp());
 }
 
-/// converting a class into widgets so inherits the StatelessWidget
-class HelloWorldApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 8,
-          title: Text('Hasan App'),
-        ),
-        body: Text('Hey I am Hasan Ahmad. This is my new flutter app project'),
+      home: AppsHome(),
+    );
+  }
+}
+
+class AppsHome extends StatelessWidget {
+  const AppsHome({Key? key}) : super(key: key);
+
+  /// SnackBar message code
+  SnackBarMessage(message, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      /// AppBar
+      appBar: AppBar(
+        /// appBar
+        title: Text("Mess App"),
+        //titleSpacing: 15,
+        centerTitle: true,
+        toolbarOpacity: 1,
+        toolbarHeight: 85,
+        elevation: 25,
+        backgroundColor: Colors.blueAccent,
+
+        /// set action icon in appbar
+        actions: [
+          IconButton(
+            onPressed: () {
+              SnackBarMessage('Search', context);
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+              onPressed: () {
+                SnackBarMessage('Call', context);
+              },
+              icon: Icon(Icons.add_call)),
+          IconButton(
+              onPressed: () {
+                SnackBarMessage('Settings', context);
+              },
+              icon: Icon(Icons.settings_applications))
+        ],
       ),
+
+      /// floating action button
+      floatingActionButton: FloatingActionButton(
+        elevation: 2,
+        child: Icon(Icons.camera_alt_outlined),
+        onPressed: () {
+          SnackBarMessage('This is Camera action', context);
+        },
+        backgroundColor: Colors.blueAccent,
+        hoverColor: Colors.blueGrey,
+      ),
+
     );
   }
 }
