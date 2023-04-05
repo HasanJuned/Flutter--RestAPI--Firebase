fe1518ba911ddd0b708ca6a6615f17a1857954f7
Hassan <hasanahmad9812021@gmail.com>
2023-04-05 19:59:10 +0600

    Firebase Setup Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -29,8 +34,48 @@ class BookListScreen extends StatefulWidget {
 }
 
 class _BookListScreenState extends State<BookListScreen> {
+
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance; /// --> Collection of Database
+  List<Book> books = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getBookList();
+  }
+
+  Future<void> getBookList() async{
+    firebaseFirestore.collection('books').get().then((documents) {
+      for(var doc in documents.docs){
+        log(doc.get('name'));
+      }
+
+    }); /// access the collection from firebase }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Books Collection'),
+      ),
+
+      body: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('Title'),
+              subtitle: Text('Writter'),
+              trailing: Text('2001'),
+            );
+          }),
+    );
   }
+
+}
+
+class Book{
+  final String name, writterName, year;
+
+  Book(this.name, this.writterName, this.year);
 }
