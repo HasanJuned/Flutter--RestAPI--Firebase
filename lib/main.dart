25596c8229b142217be5543bfe8bbc94276b3487
Hassan <hasanahmad9812021@gmail.com>
2023-04-05 19:59:10 +0600

    Get data from firebase firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -29,8 +32,61 @@ class BookListScreen extends StatefulWidget {
 }
 
 class _BookListScreenState extends State<BookListScreen> {
+
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance; /// --> Collection of Database
+  List<Book> books = [];
+  bool inProgress = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getBookList();
+  }
+
+  Future<void> getBookList() async {
+    inProgress = true;
+    setState(() {});
+    books.clear();
+    await firebaseFirestore.collection('books').get().then((documents) { /// access the collection from firebaseFirestore }
+      for (var doc in documents.docs) {
+        books.add(Book(doc.get('name'), doc.get('writter'), doc.get('year')));
+      }
+    });
+
+    inProgress = false;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Books Collection'),
+      ),
+      body: inProgress
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : RefreshIndicator(
+              onRefresh: () async {
+                getBookList();
+              },
+              child: ListView.builder(
+                  itemCount: books.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(books[index].name),
+                      subtitle: Text(books[index].authorName),
+                      trailing: Text(books[index].year),
+                    );
+                  }),
+            ),
+    );
   }
+}
+
+class Book {
+  final String name, authorName, year;
+
+  Book(this.name, this.authorName, this.year);
 }
