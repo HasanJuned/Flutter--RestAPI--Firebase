ab42bda1873285cdc716fcbeebc49dd3018e033e
Hassan <hasanahmad9812021@gmail.com>
2023-05-03 21:57:34 +0600

    Splash Screen UI added

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../utils/app_colors.dart';
-import 'email_verification_screen.dart';
+import 'bottom_nav_bar_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 2)).then((value) {
-      Get.off(const EmailVerificationScreen());
+      Get.off(const BottomNavBarScreen());
     });
   }
 
