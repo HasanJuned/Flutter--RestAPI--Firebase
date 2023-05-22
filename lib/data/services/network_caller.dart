d813ebfe34cadcc596c0de8b879852fe79d7e985
Hassan <hasanahmad9812021@gmail.com>
2023-05-22 23:50:08 +0600

    Network caller integration added

--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 
 import 'package:http/http.dart';
 
+
+import '../../ui/state_managers/auth_controller.dart';
 import '../models/response_model.dart';
 import '../utils/urls.dart';
 
@@ -11,27 +13,28 @@ class NetworkCaller {
 
   static Future<ResponseModel> getRequest({required String url}) async {
     try {
-      final Response response = await get(Uri.parse(Urls.baseUrl + url));
+      log(AuthController.token.toString());
+      final Response response = await get(Uri.parse(Urls.baseUrl + url), headers: {
+        'Content-type' : 'application/json',
+        'Accept' : 'application/json',
+        'token' : AuthController.token.toString(),
+      });
+      log(response.body);
       if (response.statusCode == 200) {
         return ResponseModel(
             isSuccess: true,
             statusCode: response.statusCode,
-            returnData: jsonDecode(response.body),
-        );
+            returnData: jsonDecode(response.body));
       } else {
         return ResponseModel(
-          isSuccess: false,
-          statusCode: response.statusCode,
-          returnData: jsonDecode(response.body),
-        );
+            isSuccess: false,
+            statusCode: response.statusCode,
+            returnData: jsonDecode(response.body));
       }
     } catch (e) {
       log(e.toString());
       return ResponseModel(
-        isSuccess: false,
-        statusCode: -1,
-        returnData: e.toString(),
-      );
+          isSuccess: false, statusCode: -1, returnData: e.toString());
     }
   }
 }
