713a4840e1b0db001af48cbb98d99e0828b3fb61
Hassan <hasanahmad9812021@gmail.com>
2023-05-22 23:51:47 +0600

    Product stepper widget added

---
+++ lib/ui/widgets/product_stepper_widget.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:ostad_flutter_batch_two/ui/utils/app_colors.dart';
+
+class ProductStepperWidget extends StatefulWidget {
+  const ProductStepperWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ProductStepperWidget> createState() => _ProductStepperWidgetState();
+}
+
+class _ProductStepperWidgetState extends State<ProductStepperWidget> {
+  int currentValue = 0;
+  late TextEditingController stepperController =
+      TextEditingController(text: '1');
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        SizedBox(
+          height: 30,
+          width: 30,
+          child: InkWell(
+            onTap: () {
+              if (currentValue > 1) {
+                currentValue--;
+                stepperController.text = currentValue.toString();
+              }
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Icon(Icons.remove),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 50,
+          height: 25,
+          child: TextField(
+            controller: stepperController,
+            enabled: false,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(borderSide: BorderSide.none),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 30,
+          width: 30,
+          child: InkWell(
+            onTap: () {
+              if (currentValue <20) {
+                currentValue++;
+                stepperController.text = currentValue.toString();
+              }
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Icon(Icons.add),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
