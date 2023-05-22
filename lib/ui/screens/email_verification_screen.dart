81d4788b360803ad8118819a8501320d8ade9492
Hassan <hasanahmad9812021@gmail.com>
2023-05-22 23:49:03 +0600

    Email verfication UI done

--- lib/ui/screens/email_verification_screen.dart
+++ lib/ui/screens/email_verification_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ostad_flutter_batch_two/ui/state_managers/user_auth_controller.dart';
 
 import '../utils/styles.dart';
 import '../widgets/common_elevated_button.dart';
@@ -15,50 +16,78 @@ class EmailVerificationScreen extends StatefulWidget {
 }
 
 class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(24.0),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Image.asset(
-          'assets/images/logo.png',
-          height: 80,
-          width: 80,
+        body: GetBuilder<UserAuthController>(builder: (authController) {
+      return Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Form(
+          key: _formKey,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Image.asset(
+              'assets/images/logo.png',
+              height: 80,
+              width: 80,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Welcome Back',
+              style: titleTextStyle,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              'Please Enter Your Email Address',
+              style: subTitleTextStyle,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            CommonTextField(
+              hintText: 'Enter your email',
+              validator: (String? value) {
+                if (value?.isEmpty ?? true) {
+                  return 'Enter a valid email';
+                }
+                return null;
+              },
+              controller: _emailController,
+              textInputType: TextInputType.emailAddress,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            authController.emailVerificationInProgress
+                ? CircularProgressIndicator()
+                : CommonElevatedButton(
+                    title: 'Next',
+                    onTap: () async {
+                      if (_formKey.currentState!.validate()) {
+                        final bool response = await authController
+                            .emailVerification(_emailController.text);
+                        if (response) {
+                          Get.to(OTPVerificationScreen(email: _emailController.text,));
+                        } else {
+                          if (mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text(
+                                        'Email Verification Failed! Try again!')));
+                          }
+                        }
+                      }
+                    },
+                  ),
+          ]),
         ),
-        const SizedBox(
-          height: 16,
-        ),
-        Text(
-          'Welcome Back',
-          style: titleTextStyle,
-        ),
-        const SizedBox(
-          height: 4,
-        ),
-        Text(
-          'Please Enter Your Email Address',
-          style: subTitleTextStyle,
-        ),
-        const SizedBox(
-          height: 24,
-        ),
-        CommonTextField(
-          hintText: 'Enter your email',
-          validator: (String? value) {},
-          controller: TextEditingController(),
-          textInputType: TextInputType.emailAddress,
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        CommonElevatedButton(
-          title: 'Next',
-          onTap: () {
-            Get.to(const OTPVerificationScreen());
-          },
-        ),
-      ]),
-    ));
+      );
+    }));
   }
 }
