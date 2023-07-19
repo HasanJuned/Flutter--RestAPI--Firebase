12528f4655d6fda9ac03a487da22b5eb34299e9c
Hassan <hasanahmad9812021@gmail.com>
2023-07-19 21:46:45 +0600

    Main & Intro file

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,54 @@
-import 'package:flutter/material.dart';
-
-main(){
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AppsHome(),
-    );
-  }
-}
-
-class AppsHome extends StatelessWidget {
-  const AppsHome({Key? key}) : super(key: key);
-
-  // SnackBar message code
-  SnackBarMessage(message,context){
-    return ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message))
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter with Hasan"),
-        //titleSpacing: 15,
-        centerTitle: true,
-        toolbarOpacity: 1,
-        toolbarHeight: 85,
-        elevation: 25,
-        backgroundColor: Colors.blueAccent,
-
-        // set action icon in appbar
-        actions: [
-          IconButton(onPressed: (){
-            SnackBarMessage('Search',context);},
-              icon: Icon(Icons.search)),
-          IconButton(onPressed: (){
-            SnackBarMessage('Call', context);},
-              icon: Icon(Icons.add_call)),
-          IconButton(onPressed: (){
-            SnackBarMessage('Settings', context);},
-              icon: Icon(Icons.settings_applications))
-        ],
-      ),
-
-      body: Center(
-        child: Text('Welcome to learning flutter with me'),
-      ),
-    );
-  }
-}
+/// This repo has about 50+ branches. Please visit these branches for updated code...
+//
+/// Topics : (and also many topics I wrote inside the branches and projects but I don't show in branches. But...
+/// If you learn attentively then you can find out many other widgets. Thank you...)
+//
+/// 1. MaterialApp - Scaffold
+/// 2. AppBar
+/// 3. Floating action button
+/// 4. Bottom navigation bar
+/// 6. Navigation drawer
+/// 7. Row - Column
+/// 8. Container
+/// 9. Button
+/// 10. Simple form and alert dialog
+/// 11. Radio - Checkbox
+/// 12. TextFormField
+/// 13. Navigation / Routing screens
+/// 14. Circular Progress Indicator
+/// 15. Linear progress Indicator
+/// 16. Circle Avatar (set profile pic)
+/// 17. ListTile
+/// 18. ListView
+/// 19. MediaQuery
+/// 20. LayoutBuilder
+/// 21. AspectRatio
+/// 22. Orientation
+/// 23. Fractionally SizedBox
+/// 24. Expanded / Flexible
+/// 25. TabBar
+/// 26. Stateful Widget Lifecycle
+/// 27. Counter App
+/// 28. showModal Bottom sheet
+/// 29. TODO app
+/// 30. Time picker
+/// 31. Date picker
+/// 32. Date Range picker
+/// 33. Rest Api intro
+/// 34. Rest Api GET
+/// 35. Rest Api POST
+/// 36. Task Manager App
+/// 37. GetX Routing
+/// 38. GetX State Management
+/// 39. Firebase
+/// 40. Firebase (realtime)
+/// 41. Firebase (storage)
+/// 42. Firebase (messaging)
+/// 43. Football Scorer made by Firebase real time database
+/// 44. Google Map
+/// 45. E commerce App
+/// 46. Quiz App
+/// 47. Food App
+///
+/// and If any update coming in my any branch or topic I will write here just simply that...
+/// for example "Updated Branch (branch name)"
